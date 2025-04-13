67dd26d3a5d21112a7c01b610f4a039564dd7b70
Franklin <franklinpereira98@hotmail.com>
2025-04-13 19:54:36 -0300

    Add Pizza Model

---
+++ src/main/java/com/hyuse/pizzaOrderingBackend/domain/product/Pizza.java
@@ -0,0 +1,26 @@
+package com.hyuse.pizzaOrderingBackend.domain.product;
+
+import com.hyuse.pizzaOrderingBackend.domain.enums.PizzaBorders;
+import com.hyuse.pizzaOrderingBackend.domain.enums.PizzaFlavor;
+import com.hyuse.pizzaOrderingBackend.domain.enums.PizzaSizes;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+
+@Getter
+@Entity
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PizzaFlavor pizzaFlavor;
+
+    private String description;
+
+    private BigDecimal price;
+}
